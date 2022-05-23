6d6a0c0c6e2163dff7f3b22a1b57b6ed9fbbb2b0
na0kiB <na0ki199823@gmail.com>
2022-05-24 03:30:05 +0900

    add env

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -68,4 +68,6 @@ Rails.application.configure do
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
   config.hosts << 'localhost'
+  config.hosts << ENV['API_HOST']
+  config.hosts << '127.0.0.1'
 end
