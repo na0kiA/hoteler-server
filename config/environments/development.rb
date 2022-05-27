b55cf2757a3cacc583cca2d34414fadc82060e87
na0kiB <na0ki199823@gmail.com>
2022-05-28 02:21:49 +0900

    before docker

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -67,7 +67,7 @@ Rails.application.configure do
 
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
-  config.hosts << 'localhost'
   config.hosts << ENV['API_HOST']
-  config.hosts << '127.0.0.1'
+  config.hosts << ENV['NGROK_HOST']
+  # config.hosts << '127.0.0.1'
 end
