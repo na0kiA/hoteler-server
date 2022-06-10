6957515d6857cf9f9645c6ed9cf263a28fb055f3
na0kiB <na0ki199823@gmail.com>
2022-06-10 23:59:07 +0900

    admin

--- config/initializers/rails_admin.rb
+++ config/initializers/rails_admin.rb
@@ -1,6 +1,12 @@
 RailsAdmin.config do |config|
   config.asset_source = :sprockets
 
+  config.authenticate_with do
+    authenticate_or_request_with_http_basic('Site Message') do |username, password|
+      username == 'admin' && password == 'admin'
+    end
+  end
+
   ### Popular gems integration
 
   ## == Devise ==
