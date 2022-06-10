71430acf368e18d01235d0d97635960108eb5caa
na0kiB <na0ki199823@gmail.com>
2022-06-11 00:14:47 +0900

    admin

--- config/initializers/rails_admin.rb
+++ config/initializers/rails_admin.rb
@@ -3,7 +3,7 @@ RailsAdmin.config do |config|
 
   config.authenticate_with do
     authenticate_or_request_with_http_basic('Site Message') do |username, password|
-      username == 'na0ki' && password == 'na0ki'
+      username == ENV['USER_NAME'] && password == ENV['USER_PASSWORD']
     end
   end
 
