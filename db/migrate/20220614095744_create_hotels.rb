f7aae2b5fbae7c4e4e08d1a841ea8c1e34c00529
na0kiB <na0ki199823@gmail.com>
2022-06-14 20:07:24 +0900

    [追加]hotels_migrate実行 #3

--- db/migrate/20220614095744_create_hotels.rb
+++ db/migrate/20220614095744_create_hotels.rb
@@ -1,8 +1,11 @@
 class CreateHotels < ActiveRecord::Migration[7.0]
   def change
     create_table :hotels do |t|
-      t.belongs_to :user, foreign_key: true
-      t.string :name, null: false, pre
+      t.references :user, foreign_key: true
+      t.string :name, null: false
+      t.text :content, null: false
+      t.string :image
+      t.boolean :accepted, default: false, null: false
       t.timestamps  
     end
   end
