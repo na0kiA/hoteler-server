b6753862752feb6c91ca71a6c8689882f255fa65
na0kiB <na0ki199823@gmail.com>
2022-06-17 21:48:31 +0900

    [追加]rspec request Hotel model rspec #3

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,5 @@
 class ApplicationController < ActionController::Base
-  rescue_from ActiveRecord::RecordNotFound, with: :record_not_found
+  # rescue_from ActiveRecord::RecordNotFound, with: :record_not_found
   include DeviseTokenAuth::Concerns::SetUserByToken
   skip_before_action :verify_authenticity_token, if: :devise_controller?, raise: false
 
@@ -7,7 +7,7 @@ class ApplicationController < ActionController::Base
 
   private
 
-  def record_not_found
-    render plain: "404 Not Found", status: :not_found
-  end
+  # def record_not_found
+  #   render plain: "404 Not Found", status: :not_found
+  # end
 end
