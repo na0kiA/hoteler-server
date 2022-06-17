567a28be6fe3c56763782f207d4cd845563e52dd
na0kiB <na0ki199823@gmail.com>
2022-06-17 16:15:29 +0900

    [追加]rspec request test #3

--- spec/requests/v1/hotels_spec.rb
+++ spec/requests/v1/hotels_spec.rb
@@ -6,8 +6,8 @@ RSpec.describe "V1::Hotels", type: :request do
       it "POSTができること" do
         client_user = create(:user)
         auth_tokens = client_user.create_new_auth_token
-        hotel = client_user.hotels.create(name: "sss", content: "sss")
-        params = { hotel: }
+        hotel = client_user.hotels.build(name: "sss", content: "sss")
+        params = { hotel: hotel}
         post v1_hotels_path, params: params, headers: auth_tokens
         expect(response).to have_http_status :ok
       end
