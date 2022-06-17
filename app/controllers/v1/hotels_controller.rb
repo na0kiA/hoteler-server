6755e162f775fed109bdddf992df9c5bca0e80e7
na0kiB <na0ki199823@gmail.com>
2022-06-17 15:12:46 +0900

    [改善]rspec request test #3

--- app/controllers/v1/hotels_controller.rb
+++ app/controllers/v1/hotels_controller.rb
@@ -1,5 +1,28 @@
 module V1
   class HotelsController < ApplicationController
-    before_action :authenticate_v1_user!, only: [create, update, destroy]
+    before_action :authenticate_v1_user!, except: %i[index show]
+
+    def index
+      render json: Hotel.all
+    end
+
+    def show
+      render json: Hotel.find(params[:id])
+    end
+
+    def create
+      hotel = Hotel.new(hotel_params)
+      if hotel.save
+        render json: hotel, status: :ok
+      else
+        render json: hotel.errors, status: :bad_request
+      end
+    end
+
+    private
+
+    def hotel_params
+      params.permit(:name, :content).merge(user_id: current_v1_user.id)
+    end
   end
 end
--- spec/requests/v1/hotels_spec.rb
+++ spec/requests/v1/hotels_spec.rb
@@ -4,7 +4,12 @@ RSpec.describe "V1::Hotels", type: :request do
   describe "POST /create" do
     context "ログインしている場合" do
       it "POSTができること" do
-        
+        client_user = create(:user)
+        auth_tokens = client_user.create_new_auth_token
+        hotel = client_user.hotels.create(name: "sss", content: "sss")
+        params = { hotel: }
+        post v1_hotels_path, params: params, headers: auth_tokens
+        expect(response).to have_http_status :ok
       end
     end
   end
