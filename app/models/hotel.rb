5785c66db32caebc94d7ef024bf058beddd5bf7c
na0kiB <na0ki199823@gmail.com>
2022-06-25 22:35:07 +0900

    [update]devcontainer#5

--- app/models/hotel.rb
+++ app/models/hotel.rb
@@ -2,6 +2,6 @@ class Hotel < ApplicationRecord
   scope :accepted_hotel, -> { where(accepted: false) }
 
   belongs_to :user
-  validates :name, presence: true, length: {maximum: 50}
+  validates :name, presence: true, length: { maximum: 50 }
   validates :content, presence: true, length: {maximum: 2000}
 end
