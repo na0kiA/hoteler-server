b23fe19f2646e2a5bc3dcf62eada83e1c9bff687
na0kiB <na0ki199823@gmail.com>
2022-06-27 17:50:12 +0900

    [update]acceptedがtrueのときindexとshowに表示 && rails_admin日本語化#5

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,8 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+10.times do |n|
+  Hotel.create!(
+    name: "ホテル#{n + 1}",
+    content: "ホテル情報#{n + 1}",
+    user_id: 1,
+    accepted: false
+  )
+end
