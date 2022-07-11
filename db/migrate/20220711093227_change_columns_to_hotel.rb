e2c3827b49b9f9d425f24f8960731ce633da2167
na0kiB <na0ki199823@gmail.com>
2022-07-12 00:37:13 +0900

    [追加]hotelsテーブルにimagesテーブルのキをー追加 #7

--- db/migrate/20220711093227_change_columns_to_hotel.rb
+++ db/migrate/20220711093227_change_columns_to_hotel.rb
@@ -1,10 +1,10 @@
 class ChangeColumnsToHotel < ActiveRecord::Migration[7.0]
-    def up
-      add_reference :hotels, :image, foreign_key: true
-      change_column :hotels, :image_id, :bigint, null: false
-    end
+  def up
+    add_reference :hotels, :image, foreign_key: true
+    change_column :hotels, :image_id, :bigint, null: false
+  end
 
-    def down
-      change_column :hotels, :image, :string, null: false
-    end
+  def down
+    change_column :hotels, :image, :string, null: false
+  end
 end
