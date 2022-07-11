013ee27ca0664df29a02e7b40f98c5a2e6653459
na0kiB <na0ki199823@gmail.com>
2022-07-12 00:27:47 +0900

    [fix]コントローラー名を変更&[add]hotelsテーブルにimagesテーブルの外部キーを追 加#7

---
+++ db/migrate/20220711093227_change_columns_to_hotel.rb
@@ -0,0 +1,10 @@
+class ChangeColumnsToHotel < ActiveRecord::Migration[7.0]
+    def up
+      add_reference :hotels, :image, foreign_key: true
+      change_column :hotels, :image_id, :bigint, null: false
+    end
+
+    def down
+      change_column :hotels, :image, :string, null: false
+    end
+end
