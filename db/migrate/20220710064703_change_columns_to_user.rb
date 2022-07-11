8509ef9d88f602f4d8b7f49a3f73b4e8bea36103
na0kiB <na0ki199823@gmail.com>
2022-07-12 00:41:20 +0900

    [add]usersテーブルにimageテーブルの外部キーを追加 #7

--- db/migrate/20220710064703_change_columns_to_user.rb
+++ db/migrate/20220710064703_change_columns_to_user.rb
@@ -1,5 +1,5 @@
 class ChangeColumnsToUser < ActiveRecord::Migration[7.0]
   def change
-    add_reference  :users, :image, foreign_key: true
+    add_reference :users, :image, foreign_key: true
   end
 end
