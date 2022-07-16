f87c82ee3f8b39be2e081d87c66298ef1ffca6b9
na0kiB <na0ki199823@gmail.com>
2022-07-17 00:03:56 +0900

    [add]削除時にnilを許容するためHotelsテーブルのimage_idカラムのNOT NULL制約を解除#9

--- db/migrate/20220711093227_change_columns_to_hotel.rb
+++ db/migrate/20220711093227_change_columns_to_hotel.rb
@@ -1,10 +1,11 @@
 class ChangeColumnsToHotel < ActiveRecord::Migration[7.0]
   def up
     add_reference :hotels, :image, foreign_key: true
-    change_column :hotels, :image_id, :bigint, null: false
   end
 
   def down
-    change_column :hotels, :image, :string, null: false
+    # change_column :hotels, :image, :string, null: false
+    remove_column :hotels, :image, :string
+    remove_column :hotels, :image_id, :bigint
   end
 end
