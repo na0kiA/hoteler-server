e4935acbb9716c81f854867323a932933dcfef42
na0kiB <na0ki199823@gmail.com>
2022-07-18 22:30:48 +0900

    [dev]hotelに保存できるようにメソッドを定義#9

--- app/controllers/v1/images_controller.rb
+++ app/controllers/v1/images_controller.rb
@@ -1,21 +1,21 @@
 module V1
   class ImagesController < ApplicationController
-    before_action :authenticate_v1_user!
+    # before_action :authenticate_v1_user!
 
     def signed_url
       resource = S3_BUCKET.presigned_post(
-        key: "uploads/test/#{SecureRandom.uuid}/${filename}", success_action_status: '201', acl: 'public-read', content_length_range: 1..(10.megabytes)
+        key: "uploads/hoteler/#{SecureRandom.uuid}/${filename}", success_action_status: '201', acl: 'public-read', content_length_range: 1..(10.megabytes)
       )
       render json: { url: resource.url, fields: resource.fields }
     end
 
     def save_hotel_key
       key = Image.new(hotel_s3_key)
-      # if key.save && key.present?
-      #   render json: key, status: :ok
-      # else
-      #   render json: key.errors, status: :bad_request
-      # end
+      key&.save
+    end
+
+    def save_user_key
+      key = Image.new(user_s3_key)
       key&.save
     end
 
@@ -26,12 +26,15 @@ module V1
     private
 
     def hotel_s3_key
-      params.require(:image).permit(:hotel_s3_key).merge(user_id: current_v1_user.id)
+      params.require(:image).permit(:hotel_s3_key)
     end
 
-    def hotel_key
+    def user_s3_key
+      params.require(:image).permit(:user_s3_key)
+    end
+
+    def image_id
       Image.find(params[:id])
     end
-
   end
 end
