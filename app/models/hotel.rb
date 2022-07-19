87107f80911bb29c489c1667e7b73baa1c98b34b
na0kiB <na0ki199823@gmail.com>
2022-07-20 02:49:44 +0900

    [dev]imagesテーブルの関連付けを追加#9

--- app/models/hotel.rb
+++ app/models/hotel.rb
@@ -2,6 +2,8 @@ class Hotel < ApplicationRecord
   scope :accepted, -> { where(accepted: true) }
 
   belongs_to :user
+  has_one :image
+  # belongs_to :image
   validates :name, presence: true, length: { maximum: 50 }
   validates :content, presence: true, length: { maximum: 2000 }
 end
