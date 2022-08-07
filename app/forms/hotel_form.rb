b6887bf9c5aee8292503b19ccdfd4f9bb4390a6f
na0kiB <na0ki199823@gmail.com>
2022-08-07 15:06:20 +0900

    [fix]hotelformでsaveができるように変更#11

--- app/forms/hotel_form.rb
+++ app/forms/hotel_form.rb
@@ -4,18 +4,20 @@ class HotelForm
 
   attribute :name, :string
   attribute :content, :string
-  attribute :hotel_images
   attribute :user_id, :integer
+  attribute :key, :string
+  attribute :file_url, :string
 
   with_options presence: true do
-    validates :hotel_images
     validates :user_id
+    validates :name, length: { maximum: 50 }
+    validates :content, length: { minimum: 10, maximum: 2000 }
+    validates :key
+    validates :file_url
   end
 
-  validates :name, presence: true, length: { maximum: 50 }
-  validates :content, presence: true, length: { minimum: 10, maximum: 2000 }
-
-  def params
-    attributes.deep_symbolize_keys
+  def save(params)
+    hotel = Hotel.create!(name: params[:name], content: params[:content], user_id: params[:user_id])
+    HotelImage.create!(hotel_id: hotel.id, key: params[:key], file_url: params[:file_url])
   end
 end
