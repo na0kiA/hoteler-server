9bd505b99b7244877ccbb89cab14807e71811d31
na0kiB <na0ki199823@gmail.com>
2022-08-08 22:43:38 +0900

    [dev]hotel_formのメソッドが正しく動作するか検証#11

--- spec/models/hotel_form_spec.rb
+++ spec/models/hotel_form_spec.rb
@@ -34,13 +34,27 @@ RSpec.describe HotelForm, type: :model do
   describe "models/hotel_form.rb #save" do
     let_it_be(:user) { create(:user) }
     context "正常に保存ができる場合" do
-      def save(params)
-        hotel = Hotel.create!(name: params[:name], content: params[:content], user_id: params[:user_id])
-        HotelImage.create!(hotel_id: hotel.id, key: params[:key], file_url: params[:file_url])
+      it "saveがparamsを受け取って実行できていること" do
+        params = { name: "hotelName", content: "hotelContent", key: "upload/test", file_url: "https://example/aws/s3", user_id: user.id }
+        s = spy
+        s.save(params)
+        expect(s).to have_received(:save).with(name: "hotelName", content: "hotelContent", key: "upload/test", file_url: "https://example/aws/s3", user_id: user.id)
+
+        hotel = { name: params[:name], content: params[:content], user_id: params[:user_id] }
+
+        expect { Hotel.create!(hotel) }.to change(Hotel, :count).by(1)
+        hotel_form = Hotel.create!(hotel)
+        image = { hotel_id: hotel_form.id, key: params[:key], file_url: params[:file_url] }
+
+        expect { HotelImage.create!(image) }.to change(HotelImage, :count).by(1)
+
+        # hotel_form = HotelForm.new(params)
+        # allow(hotel_form).to receive(:save).and_return(hotel_form)
+        # expect(hotel_form.save(params)).to include("name")
       end
-      it "HotelとHotelImageを保存できること" do
-        # expect(Hotel).to receive(:)
-      end
+    end
+
+    context "保存ができない場合" do
     end
   end
 end
