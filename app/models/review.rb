3513bd81c0b14c90b29dd0914e39609ac8667352
na0kiB <na0ki199823@gmail.com>
2022-08-10 01:45:56 +0900

    [clean]不必要なコメントとファイルを削除#11

--- app/models/review.rb
+++ app/models/review.rb
@@ -4,4 +4,10 @@ class Review < ApplicationRecord
 
   validates :title, presence: true, length: { minimum: 5, maximum: 50 }
   validates :content, presence: true, length: { minimum: 10, maximum: 1000 }
+
+  # class << self
+  #   def save(params)
+  #     Review.create!(title: params[:title], content: params[:content], hotel_id: params[:hotel_id], user_id: params[:user_id])
+  #   end
+  # end
 end
--- spec/factories/hotel_forms.rb
+++
@@ -1,4 +0,0 @@
-FactoryBot.define do
-  factory :hotel_form do
-  end
-end
