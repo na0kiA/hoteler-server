89b2f259b9ed3fed27cb1ff1912f248fdfe1dd04
na0kiB <na0ki199823@gmail.com>
2022-08-13 01:43:45 +0900

    [dev]エラーハンドリングをするためカスタムエラーを追加#11

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,8 @@
 class ApplicationController < ActionController::Base
   rescue_from ActiveRecord::RecordNotFound, with: :record_not_found
+  rescue_from ActionController::RoutingError, with: :path_not_found
+  rescue_from User::NotAuthorized, with: :user_not_authorized
+
   include DeviseTokenAuth::Concerns::SetUserByToken
   skip_before_action :verify_authenticity_token
 
@@ -9,5 +12,18 @@ class ApplicationController < ActionController::Base
 
   def record_not_found
     render json: { errors: '404 not found' }, status: :not_found
+  end
+
+  def path_not_found
+    render json: { errors: '存在しないページです' }, status: :not_found
+  end
+
+  def user_not_authorized
+    render json: { errors: 'アクセス権限がありません' }, status: :forbidden
+    redirect_back(fallback_location: root_path)
+  end
+
+  def render_json_bad_request(title, detail)
+    render json: { errors: { title: title, detail: detail } }, status: :bad_request
   end
 end
