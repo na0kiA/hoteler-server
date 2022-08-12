de1b01c8467430fd3308ccedef72cb206abcca79
na0kiB <na0ki199823@gmail.com>
2022-08-13 02:47:28 +0900

    [fix]存在しないカスタムエラーを消去#11

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,6 @@
 class ApplicationController < ActionController::Base
   rescue_from ActiveRecord::RecordNotFound, with: :record_not_found
   rescue_from ActionController::RoutingError, with: :path_not_found
-  rescue_from User::NotAuthorized, with: :user_not_authorized
 
   include DeviseTokenAuth::Concerns::SetUserByToken
   skip_before_action :verify_authenticity_token
@@ -16,11 +15,6 @@ class ApplicationController < ActionController::Base
 
   def path_not_found
     render json: { errors: '存在しないページです' }, status: :not_found
-  end
-
-  def user_not_authorized
-    render json: { errors: 'アクセス権限がありません' }, status: :forbidden
-    redirect_back(fallback_location: root_path)
   end
 
   def render_json_bad_request(title, detail)
