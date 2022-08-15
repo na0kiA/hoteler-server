3a5105e49f505a863f92426daf91448cb13c19a6
na0kiB <na0ki199823@gmail.com>
2022-08-16 01:48:31 +0900

    [fix]keyとfileurlのvalidationを変更#13

--- app/forms/hotel_form.rb
+++ app/forms/hotel_form.rb
@@ -12,8 +12,8 @@ class HotelForm
     validates :user_id
     validates :name, length: { maximum: 50 }
     validates :content, length: { minimum: 10, maximum: 2000 }
-    validates :key
-    validates :file_url
+    validates :key, length: { minimum: 10 }
+    validates :file_url, length: { minimum: 10 }
   end
 
   def save(params)
