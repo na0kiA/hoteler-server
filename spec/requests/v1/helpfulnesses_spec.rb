48c958df315348ce5bbcca35442f1b2dfeca17b9
na0kiB <na0ki199823@gmail.com>
2022-08-16 21:14:12 +0900

    [dev]helpfulness requestのテストを追加#13

---
+++ spec/requests/v1/helpfulnesses_spec.rb
@@ -0,0 +1,79 @@
+require 'rails_helper'
+
+RSpec.describe 'V1::Helpfulnesses', type: :request do
+  describe 'POST /v1/reviews/:review_id/helpfulnesses - v1/helpfulness#create' do
+    let_it_be(:client_user)  { create(:user) }
+    let_it_be(:auth_tokens)  { client_user.create_new_auth_token }
+    let_it_be(:accepted_hotel) { create(:accepted_hotel, user_id: client_user.id) }
+    let_it_be(:review) { create(:review, user_id: client_user.id, hotel_id: accepted_hotel.id) }
+
+    context '参考になったを押せる場合' do
+      it 'DBに保存ができること' do
+        expect { post v1_helpfulnesses_path(review_id: review.id), headers: auth_tokens }.to change(Helpfulness, :count).by(1)
+      end
+
+      it 'statusが200なこと' do
+        post v1_helpfulnesses_path(review_id: review.id), headers: auth_tokens
+        expect(response.status).to eq(200)
+      end
+    end
+
+    context '参考になったを押せない場合' do
+      it 'ログインしていないとき、401エラーがでること' do
+        expect { post v1_helpfulnesses_path(review_id: review.id), headers: }.not_to change(Helpfulness, :count)
+        post v1_helpfulnesses_path(review_id: review.id), headers: nil
+        response_body = JSON.parse(response.body, symbolize_names: true)
+        expect(response.status).to eq(401)
+        expect(response_body[:errors][0]).to include('ログインもしくはアカウント登録してください')
+      end
+
+      it '口コミが存在しないとき、エラーが表示されること' do
+        post v1_helpfulnesses_path(0), headers: auth_tokens
+        expect(response).to have_http_status(:bad_request)
+        expect(response.message).to include('Bad Request')
+        expect(JSON.parse(response.body)['errors']['title']).to include('存在しない口コミです')
+      end
+    end
+
+    context '既に押している場合' do
+      it 'destroyに内部リダイレクトされること' do
+        expect { post v1_helpfulnesses_path(review_id: review.id), headers: auth_tokens }.to change(Helpfulness, :count).by(1)
+        post v1_helpfulnesses_path(review_id: review.id), headers: auth_tokens
+        expect(response.status).to eq(302)
+      end
+    end
+  end
+
+  describe 'DELETE /v1/reviews/:review_id/helpfulnesses - v1/helpfulness#destroy' do
+    let_it_be(:client_user)  { create(:user) }
+    let_it_be(:auth_tokens)  { client_user.create_new_auth_token }
+    let_it_be(:accepted_hotel) { create(:accepted_hotel, user_id: client_user.id) }
+    let_it_be(:review) { create(:review, user_id: client_user.id, hotel_id: accepted_hotel.id) }
+
+    context 'deleteができる場合' do
+      let_it_be(:helpfulness) { Helpfulness.create(user_id: client_user.id, review_id: review.id) }
+
+      it '200が帰ってきてdeleteできること' do
+        delete v1_helpfulnesses_path(review_id: review.id), headers: auth_tokens
+        expect(response.status).to eq(200)
+      end
+    end
+
+    context 'ログインをしていない場合' do
+      let_it_be(:helpfulness) { Helpfulness.create(user_id: client_user.id, review_id: review.id) }
+      it '401が帰ってくること' do
+        delete v1_helpfulnesses_path(review_id: review.id), headers: nil
+        expect(response.status).to eq(401)
+        expect(response.message).to include('Unauthorized')
+      end
+    end
+
+    context '「参考になった」を押していない場合' do
+      it '400とカスタムエラーが帰ってくること' do
+        delete v1_helpfulnesses_path(review_id: review.id), headers: auth_tokens
+        expect(response.status).to eq(400)
+        expect(JSON.parse(response.body)['errors']['title']).to include('「参考になった」を取り消せません')
+      end
+    end
+  end
+end
