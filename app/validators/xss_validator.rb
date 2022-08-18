9e1b021c1dd8f1c4c6e717278b9df35f0a8d9488
na0kiB <na0ki199823@gmail.com>
2022-08-18 16:21:38 +0900

    [add]activerecordの口コミのエラーを記述#13

--- app/validators/xss_validator.rb
+++
@@ -1,7 +0,0 @@
-class XssValidator < ActiveModel::Validator
-  def validate(record)
-    if options[:fields].any? { |field| record.send(field) =~ />|</ }
-      record.errors.add :base, '「<」や「>」といった記号は使用できません'
-    end
-  end
-end
