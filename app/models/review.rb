2b2242eaf7a0342acd2f8c4e56a2ad5171ffeaa4
na0kiB <na0ki199823@gmail.com>
2022-08-20 00:11:47 +0900

    [dev]カスタムバリデーションを適用#13

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,12 +1,15 @@
 class Review < ApplicationRecord
-  validates_with XssValidator, fields: [:title, :content]
 
   belongs_to :user
   belongs_to :hotel
   has_many :helpfulnesses, dependent: :destroy
 
-  validates :title, presence: true, length: { minimum: 5, maximum: 50 }
-  validates :content, presence: true, length: { minimum: 10, maximum: 1000 }
+  with_options invalid_words: true do
+    with_options presence: true do
+      validates :title, length: { minimum: 2, maximum: 1000 } 
+      validates :content, length: { minimum: 10, maximum: 1000 }
+    end
+  end
 
   class << self
     def save(params)
--- app/validators/invalid_words_validator.rb
+++ app/validators/invalid_words_validator.rb
@@ -1,17 +1,15 @@
-# InvalidWordsValidatorの部分は先ほど付けた名前をキャメルケースにしたものを使う。
 class InvalidWordsValidator < ActiveModel::EachValidator
   SAME_CHARACTER_REGEX = /(.)\1{4,}/
-  def validate_each(record, _attribute, value)
+  def validate_each(record, attribute, value)
     blacklist = YAML.load_file('./config/blacklist.yml')
-    record.errors.add(:contain_blacklist_words, '暴力的もしくは卑猥なワードが含まれています') if value.present? && blacklist.any? { |word| value.include?(word) }
+    record.errors.add(attribute, :contain_blacklist_words) if value.present? && blacklist.any? { |word| value.include?(word) }
 
-    record.errors.add(:contain_same_words, '同じ文字が5文字以上連続しています') if value.present? && value.match?(SAME_CHARACTER_REGEX)
+    record.errors.add(attribute, :contain_same_words) if value.present? && value.match?(SAME_CHARACTER_REGEX)
 
     invalid_regex = {
       url_regex: %r{https?://[\w/:%#{Regexp.last_match(0)}?()~.=+\-]+},
       html_regex: /<(".*?"|'.*?'|[^'"])*?>/
     }
-
-    record.errors.add(:contain_invalid_regex, '無効な記号やURLが含まれています') if value.present? && invalid_regex.any? { |_invalid_key, invalid_value| invalid_value.match?(value) }
+    record.errors.add(attribute, :contain_invalid_regex) if value.present? && invalid_regex.any? { |_invalid_key, invalid_value| invalid_value.match?(value) }
   end
 end
