c95d7f1c6da4f58ae9d17b9ce17edfd4e0cb1872
na0kiB <na0ki199823@gmail.com>
2022-08-21 00:29:49 +0900

    [clean]各validationをprivateにまとめてvalueで真偽値を返すようにリファクタ#13

--- app/validators/invalid_words_validator.rb
+++ app/validators/invalid_words_validator.rb
@@ -1,15 +1,31 @@
 class InvalidWordsValidator < ActiveModel::EachValidator
-  SAME_CHARACTER_REGEX = /(.)\1{4,}/
   def validate_each(record, attribute, value)
-    blacklist = YAML.load_file('./config/blacklist.yml')
-    record.errors.add(attribute, :contain_blacklist_words) if value.present? && blacklist.any? { |word| value.include?(word) }
+    return if value.blank?
 
-    record.errors.add(attribute, :contain_same_words) if value.present? && value.match?(SAME_CHARACTER_REGEX)
+    record.errors.add(attribute, :contain_invalid_regex) if url_and_sign_validator(value)
 
+    record.errors.add(attribute, :contain_same_words) if same_words_validator(value)
+
+    record.errors.add(attribute, :contain_blacklist_words) if blacklist_words_validator(value)
+  end
+
+  private
+
+  def url_and_sign_validator(value)
     invalid_regex = {
       url_regex: %r{https?://[\w/:%#{Regexp.last_match(0)}?()~.=+\-]+},
       html_regex: /<(".*?"|'.*?'|[^'"])*?>/
     }
-    record.errors.add(attribute, :contain_invalid_regex) if value.present? && invalid_regex.any? { |_invalid_key, invalid_value| invalid_value.match?(value) }
+    invalid_regex.any? { |_invalid_key, invalid_value| invalid_value.match?(value) }
+  end
+
+  def same_words_validator(value)
+    same_words_regex = /(.)\1{4,}/
+    value.match?(same_words_regex)
+  end
+
+  def blacklist_words_validator(value)
+    blacklist = YAML.load_file('./config/blacklist.yml')
+    blacklist.any? { |word| value.include?(word) }
   end
 end
