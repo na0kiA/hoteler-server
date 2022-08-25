10496f8d8781cfba0cf77200d8f715b68445fffc
na0kiB <na0ki199823@gmail.com>
2022-08-25 15:20:25 +0900

    [dev]五つ星のバリデーションを追加#15

--- app/models/review.rb
+++ app/models/review.rb
@@ -3,8 +3,9 @@ class Review < ApplicationRecord
   belongs_to :hotel
   has_many :helpfulnesses, dependent: :destroy
 
-  with_options invalid_words: true do
-    with_options presence: true do
+  with_options presence: true do
+    validates :five_star_rate, numericality: {only_integer: true,less_than_or_equal_to: 5, greater_than_or_equal_to: 1}
+    with_options invalid_words: true do
       validates :title, length: { minimum: 2, maximum: 1000 }
       validates :content, length: { minimum: 10, maximum: 1000 }
     end
