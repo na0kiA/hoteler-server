e0387908dc4bc0788e262299034a6bd19e846696
na0kiB <na0ki199823@gmail.com>
2022-08-26 21:05:10 +0900

    [fix]serializderを一旦コメントアウト#15

---
+++ app/serializers/review_serializer.rb
@@ -0,0 +1,7 @@
+# class ReviewSerializer < ActiveModel::Serializer
+#   attributes :id, :title, :content, :five_star_rate, :created_at
+
+#   belongs_to :user
+#   belongs_to :hotel
+#   has_many :helpfulnesses
+# end
