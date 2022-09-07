bb5e1522e7ecf4d02ceec7339c037c4de2383b46
na0kiB <na0ki199823@gmail.com>
2022-09-07 15:55:39 +0900

    [dev]hotelの編集時に前回保存の画像を消去してからupdateするように変更#15

--- app/models/hotel.rb
+++ app/models/hotel.rb
@@ -5,6 +5,8 @@ class Hotel < ApplicationRecord
   has_many :hotel_images, dependent: :destroy
   has_many :reviews, dependent: :destroy
 
+  # before_create :delete_hotel_images
+
   def self.save(params)
     ActiveRecord::Base.transaction do
       hotel = Hotel.new(name: params[:name], content: params[:content], user_id: params[:user_id])
@@ -18,18 +20,21 @@ class Hotel < ApplicationRecord
   end
 
   def self.update(params, set_hotel)
-    # 1, callbackでsaveする前に保存されている値をすべて削除してからbuild,saveをする
-    # 2, 
     ActiveRecord::Base.transaction do
       hotel = Hotel.update!(set_hotel.id, name: params[:name], content: params[:content])
+      hotel.hotel_images.destroy_all
       JSON.parse(params[:key]).each do |val|
-        binding.break
         hotel.hotel_images.build(key: val, file_url: params[:file_url])
-        #<HotelImage id: nil, hotel_id: 166, key: "key2001", file_url: "this is file", created_at: nil, updated_at: nil>
-        #<HotelImage id: nil, hotel_id: 166, key: "key2002", file_url: "this is file", created_at: nil, updated_at: nil>
       end
+      hotel.save!
     end
   rescue ActiveRecord::RecordInvalid
     false
   end
+
+  # private
+
+  # def delete_hotel_images
+  #   puts  "前回のホテル画像を削除"
+  # end
 end
