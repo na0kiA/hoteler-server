8a24d9aed643b03ca8568a34640c6ee05c915acc
na0kiB <na0ki199823@gmail.com>
2022-09-14 04:41:23 +0900

    [fix]file_urlを削除したため修正#15

--- spec/requests/v1/reviews_spec.rb
+++ spec/requests/v1/reviews_spec.rb
@@ -14,18 +14,18 @@ RSpec.describe 'V1::Reviews', type: :request do
       it '200を返すこと' do
         post v1_hotel_reviews_path(hotel_id: accepted_hotel.id), params: params, headers: auth_tokens
         expect(response.status).to eq(200)
-        expect(symbolized_body(response).length).to eq(4)
+        expect(symbolized_body(response).length).to eq(3)
         expect(symbolized_body(response)[:attributes]).to include(title: 'hotelName', content: 'Kobe Kitanosaka is location')
       end
     end
 
     context '画像を付けて口コミの投稿ができる場合' do
       it '200を返すこと' do
-        images_params = { review: { title: 'hotelName', content: 'Kobe Kitanosaka is location', five_star_rate: 5, key: 'upload/test', file_url: 'https://example/aws/s3' } }
+        images_params = { review: { title: 'hotelName', content: 'Kobe Kitanosaka is location', five_star_rate: 5, key: ['upload/test']} }
         post v1_hotel_reviews_path(hotel_id: accepted_hotel.id), params: images_params, headers: auth_tokens
         expect(response.status).to eq(200)
-        expect(symbolized_body(response).length).to eq(4)
-        expect(symbolized_body(response)[:attributes]).to include(key: 'upload/test', file_url: 'https://example/aws/s3')
+        expect(symbolized_body(response).length).to eq(3)
+        expect(symbolized_body(response)[:attributes][:key]).to include("[\"upload/test\"]")
       end
     end
 
@@ -78,24 +78,23 @@ RSpec.describe 'V1::Reviews', type: :request do
     end
 
     context '画像の含まれている口コミを取得できる場合' do
-      images_params = { review: { title: 'hotelName', content: 'Kobe Kitanosaka is location', five_star_rate: 5, key: 'upload/test', file_url: 'https://example/aws/s3' } }
+      images_params = { review: { title: 'お風呂が綺麗でした', content: 'また行きたいと思っています', five_star_rate: 5, key: ['upload/test']} }
 
       before do
         post v1_hotel_reviews_path(hotel_id: accepted_hotel.id), params: images_params, headers: auth_tokens
       end
 
       it '200を返すこと' do
-        # TODO review_imagesも取得できるようにする
         get v1_hotel_reviews_path(hotel_id: accepted_hotel.id)
         expect(response.status).to eq(200)
-        expect(symbolized_body(response)[:attributes][:key]).to eq('upload/test')
+        expect(symbolized_body(response)[0][:review_images][0][:key]).to eq('upload/test')
       end
     end
 
     context 'ホテルそのものが存在しない場合' do
       it '404 NOT FOUNDを返すこと' do
         unknow_hotel = create(:accepted_hotel, user_id: client_user.id)
-        delete v1_hotel_path(id: unknow_hotel.id), headers: auth_tokens
+        delete v1_hotel_path(unknow_hotel.id), headers: auth_tokens
         get v1_hotel_reviews_path(hotel_id: unknow_hotel.id)
         expect(symbolized_body(response).length).to eq(1)
         expect(response.status).to eq(404)
@@ -112,7 +111,7 @@ RSpec.describe 'V1::Reviews', type: :request do
     context '口コミを削除できる場合' do
       it '200を返すこと' do
         expect do
-          delete v1_user_review_path(id: review.id), headers: auth_tokens
+          delete v1_user_review_path(review.id), headers: auth_tokens
         end.to change(Review, :count).by(-1)
         expect(response.status).to eq(200)
       end
@@ -123,7 +122,7 @@ RSpec.describe 'V1::Reviews', type: :request do
         user = create(:user)
         other_review = create(:review, hotel_id: accepted_hotel.id, user_id: user.id)
         expect do
-          delete v1_user_review_path(id: other_review.id), headers: auth_tokens
+          delete v1_user_review_path(other_review.id), headers: auth_tokens
         end.not_to change(Review, :count)
         expect(response.status).to eq(400)
       end
@@ -132,7 +131,7 @@ RSpec.describe 'V1::Reviews', type: :request do
     context 'ログインしていない場合' do
       it '401を返すこと' do
         expect do
-          delete v1_user_review_path(id: review.id), headers: nil
+          delete v1_user_review_path(review.id), headers: nil
         end.not_to change(Review, :count)
         expect(response.status).to eq(401)
       end
@@ -148,7 +147,7 @@ RSpec.describe 'V1::Reviews', type: :request do
 
     context '自分の投稿した口コミの編集ができる場合' do
       it '200を返すこと' do
-        patch v1_user_review_path(id: review.id), params: params, headers: auth_tokens
+        patch v1_user_review_path(review.id), params: params, headers: auth_tokens
         expect(response.status).to eq(200)
         expect(symbolized_body(response)[:attributes]).to include(title: 'title uploaded', content: 'content uploaded')
       end
@@ -157,33 +156,33 @@ RSpec.describe 'V1::Reviews', type: :request do
     context '五つ星の編集ができる場合' do
       it '200を返すこと' do
         params = { review: { title: '五つ星を変えました', content: '五つ星を変えました。よかったです', five_star_rate: 2 } }
-        patch v1_user_review_path(id: review.id), params: params, headers: auth_tokens
+        patch v1_user_review_path(review.id), params: params, headers: auth_tokens
         expect(response.status).to eq(200)
-        expect(symbolized_body(response)[:review][:five_star_rate]).to eq(2)
+        expect(symbolized_body(response).length).to eq(3)
       end
     end
 
     context '画像の追加編集ができる場合' do
       it '200を返すこと' do
-        empty_images_params = { review: { title: 'hotelName', content: 'Kobe Kitanosaka is location', five_star_rate: 5, key: '', file_url: '' } }
+        empty_images_params = { review: { title: 'hotelName', content: 'Kobe Kitanosaka is location', five_star_rate: 5, key: [],} }
         post v1_hotel_reviews_path(hotel_id: accepted_hotel.id), params: empty_images_params, headers: auth_tokens
         expect(response.status).to eq(200)
 
-        added_images_params = { review: { title: 'hotelName', content: 'Kobe Kitanosaka is location', five_star_rate: 5, key: 'upload/test', file_url: 'https://example/aws/s3' } }
-        patch v1_user_review_path(id: review.id), params: added_images_params, headers: auth_tokens
+        added_images_params = { review: { title: 'hotelName', content: 'Kobe Kitanosaka is location', five_star_rate: 5, key: ['upload/test']} }
+        patch v1_user_review_path(review.id), params: added_images_params, headers: auth_tokens
         expect(response.status).to eq(200)
-        expect(symbolized_body(response)[:attributes][:file_url]).to eq('https://example/aws/s3')
+        expect(symbolized_body(response)[:attributes][:key]).to eq("[\"upload/test\"]")
       end
     end
 
     context '既存の画像の削除ができる場合' do
       it '200を返すこと' do
-        params = { review: { title: '画像を追加しました', content: '画像を追加しました。よかったです', five_star_rate: 2, key: 'upload/test', file_url: 'https://example/aws/s3' } }
+        params = { review: { title: '画像を追加しました', content: '画像を追加しました。よかったです', five_star_rate: 2, key: ['upload/test']} }
         post v1_hotel_reviews_path(hotel_id: accepted_hotel.id), params: params, headers: auth_tokens
         expect(response.status).to eq(200)
 
         delete_images_params = { review: { title: '画像を削除しました', content: '画像を削除しました。', five_star_rate: 2 } }
-        patch v1_user_review_path(id: review.id), params: delete_images_params, headers: auth_tokens
+        patch v1_user_review_path(review.id), params: delete_images_params, headers: auth_tokens
         expect(response.status).to eq(200)
         expect(symbolized_body(response)[:attributes][:title]).to eq('画像を削除しました')
         expect(symbolized_body(response)[:attributes][:file_url]).to be_nil
@@ -194,7 +193,7 @@ RSpec.describe 'V1::Reviews', type: :request do
       it '400を返すこと' do
         user = create(:user)
         different_review = create(:review, user_id: user.id, hotel_id: accepted_hotel.id)
-        patch v1_user_review_path(id: different_review.id), params: params, headers: auth_tokens
+        patch v1_user_review_path(different_review.id), params: params, headers: auth_tokens
         expect(symbolized_body(response)).not_to include(title: 'title uploaded', content: 'content uploaded')
         expect(response.status).to eq(400)
       end
@@ -202,7 +201,7 @@ RSpec.describe 'V1::Reviews', type: :request do
 
     context 'ログインしていない場合' do
       it '401を返すこと' do
-        patch v1_user_review_path(id: review.id), params: params
+        patch v1_user_review_path(review.id), params: params
         expect(response.status).to eq(401)
         expect(symbolized_body(response)).not_to include(title: 'title uploaded', content: 'content uploaded')
       end
@@ -217,7 +216,7 @@ RSpec.describe 'V1::Reviews', type: :request do
     context '口コミが書かれている場合' do
       it '200を返すこと' do
         review = create(:review, user_id: client_user.id, hotel_id: accepted_hotel.id)
-        get v1_user_review_path(id: review.id)
+        get v1_user_review_path(review.id)
         expect(response.status).to eq(200)
         expect(symbolized_body(response).length).to eq(10)
       end
@@ -227,8 +226,8 @@ RSpec.describe 'V1::Reviews', type: :request do
       it 'NOT FOUNDを返すこと' do
         unknow_hotel = create(:accepted_hotel, user_id: client_user.id)
         review = create(:review, user_id: client_user.id, hotel_id: unknow_hotel.id)
-        delete v1_hotel_path(id: unknow_hotel.id), headers: auth_tokens
-        get v1_user_review_path(id: review.id)
+        delete v1_hotel_path(unknow_hotel.id), headers: auth_tokens
+        get v1_user_review_path(review.id)
         expect(symbolized_body(response).length).to eq(1)
         expect(response.status).to eq(404)
       end
@@ -238,8 +237,8 @@ RSpec.describe 'V1::Reviews', type: :request do
       it '404 NOT FOUNDを返すこと' do
         hotel = create(:accepted_hotel, user_id: client_user.id)
         review = create(:review, user_id: client_user.id, hotel_id: hotel.id)
-        delete v1_user_review_path(id: review.id), headers: auth_tokens
-        get v1_user_review_path(id: review.id)
+        delete v1_user_review_path(review.id), headers: auth_tokens
+        get v1_user_review_path(review.id)
         expect(symbolized_body(response).length).to eq(1)
         expect(response.status).to eq(404)
       end
@@ -247,9 +246,9 @@ RSpec.describe 'V1::Reviews', type: :request do
 
     context '口コミが存在しない場合' do
       it '404 NOT FOUNDを返すこと' do
-        get v1_user_review_path(' ')
-        expect(response.status).to eq(404)
-        expect(response.message).to include('Not Found')
+        get v1_user_review_path(10**5)
+        expect(response).to have_http_status(:not_found)
+        expect(response.message).to eq('Not Found')
       end
     end
   end
