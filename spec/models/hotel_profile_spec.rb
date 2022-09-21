05ecb07815102dc58f8eeb19e00e0d5a8303c2d1
na0kiB <na0ki199823@gmail.com>
2022-09-21 15:44:43 +0900

    [fix]コメント削除#19

--- spec/models/hotel_profile_spec.rb
+++ spec/models/hotel_profile_spec.rb
@@ -16,18 +16,5 @@ RSpec.describe HotelProfile, type: :model do
         expect(hotel_profile.send(:update_hotel)[:name]).to eq('神戸北野')
       end
     end
-
-    # context '値が不正な場合' do
-    #   it "false返ること" do
-    #     params = {:name=>"", :content=>"最高峰のラグジュアリーホテルをお届けします", :key=>"[\"key1213\", \"key4561\"]", :user_id=>user.id}
-
-    #     hotel_profile = HotelProfile.new(params: params, set_hotel: accepted_hotel)
-    #     hotel_profile.instance_variable_get('@params')
-    #     expect(params[:name]).to eq(nil)
-    #     expect(hotel_profile.update).to eq(0)
-    #     expect(hotel_profile.send(:update_hotel)[:name]).to eq("")
-    #     expect{hotel_profile.send(:update_hotel)}.to raise_error(ActiveRecord::RecordInvalid)
-    #   end
-    # end
   end
 end
