7158801ad9b86cc604272db08b3eb38a2cbeb109
na0kiB <na0ki199823@gmail.com>
2022-09-29 23:32:43 +0900

    [fix]attributeモジュールのカスタムタイプを削除#19

--- app/models/type_daily_rate_array.rb
+++
@@ -1,9 +0,0 @@
-# class TypeDailyRateArray < ActiveModel::Type::Value
-#   def cast_value(value)
-#     arr = []
-#     value.each do |v|
-#       arr.push Day.new(v)
-#     end
-#     arr
-#   end
-# end
