4b14db6786bd8fcedea23d7db39ae1283b5e4973
na0kiB <na0ki199823@gmail.com>
2022-10-08 01:58:26 +0900

    [fix]factoryは一意な値にする以外でシーケンスを使わなくていいので修正#19

--- app/models/rest_rate.rb
+++ app/models/rest_rate.rb
@@ -18,37 +18,32 @@ class RestRate < ApplicationRecord
   # 今の曜日と時間から、ホテルの休憩で一番安いプランと料金を返す
   # 営業時間外(nil)の場合は"営業時間外です"をJSONで表示する
   def now_rest_rate
-    during_business_hours
-    # outside_business_hours
+    during_business_hours_list
   end
 
-  def during_business_hours
+  def during_business_hours_list
     # RestRate.where(id: rest_time_array.map(&:present?))
     RestRate.where(id: can_take_rest_array.select { |num| num.to_s.match?(/^[0-9]+$/) })
-  end
-
-  def outside_business_hours
-    RestRate.where(id: can_take_rest_array.reject { |num| num.to_s.match?(/^[0-9]+$/)})
   end
 
   private
 
     def can_take_rest_array
       today_rest_rate_list.pluck(:first_time, :last_time, :id, :plan).map do |val|
-         if val[3].include?("深夜休憩")
+        if val[3].start_with?('深夜休憩')
           can_take_a_midnight_rest_or_not(first_time: val[0], last_time: val[1], ids: val[2])
         else
-          can_take_a_rest_or_not(first_time: val[0], last_time: val[1], ids: val[2]) 
+          can_take_a_normal_rest_or_not(first_time: val[0], last_time: val[1], ids: val[2])
         end
       end
     end
 
-    def can_take_a_rest_or_not(first_time:, last_time:, ids:)
+    def can_take_a_normal_rest_or_not(first_time:, last_time:, ids:)
       at_now_includes_rest_time?(first_time: convert_at_hour(time: first_time), last_time: convert_at_hour(time: last_time)) ? ids : "#{ids}は営業時間外です"
     end
 
     def can_take_a_midnight_rest_or_not(first_time:, last_time:, ids:)
-      at_now_includes_midnight_rest_time?(first_time: convert_at_hour(time: first_time), last_time: convert_at_hour(time: last_time)) ? ids : "#{ids}は営業時間外です" 
+      at_now_includes_midnight_rest_time?(first_time: convert_at_hour(time: first_time), last_time: convert_at_hour(time: last_time)) ? ids : "#{ids}は営業時間外です"
     end
 
     def at_now_includes_rest_time?(first_time:, last_time:)
@@ -56,7 +51,7 @@ class RestRate < ApplicationRecord
     end
 
     def at_now_includes_midnight_rest_time?(first_time:, last_time:)
-      [*first_time..last_time].include?(convert_at_hour(time: Time.current)) 
+      [*first_time..last_time].include?(convert_at_hour(time: Time.current))
     end
 
     def convert_at_hour(time:)
--- spec/factories/days.rb
+++ spec/factories/days.rb
@@ -1,7 +1,22 @@
 # frozen_string_literal: true
 
 FactoryBot.define do
-  factory :day do
-    sequence(:day) { '月曜から木曜' }
+  factory :weekdays, class: 'Day' do
+    day { '月曜から木曜' }
+  end
+  factory :friday, class: 'Day' do
+    day { '金曜' }
+  end
+  factory :saturday, class: 'Day' do
+    day { '土曜' }
+  end
+  factory :sunday, class: 'Day' do
+    day { '日曜' }
+  end
+  factory :holiday, class: 'Day' do
+    day { '祝日' }
+  end
+  factory :special_weeks, class: 'Day' do
+    day { '特別期間' }
   end
 end
