83a33f913a4b32e5af3e9e1f0dd4670d2d67a906
na0kiB <na0ki199823@gmail.com>
2022-10-16 00:45:39 +0900

    [add]special_periodsの保存ができるようにロジックを記述#19

--- app/forms/hotel_form.rb
+++ app/forms/hotel_form.rb
@@ -9,7 +9,8 @@ class HotelForm
   attribute :key, :string
   attribute :user_id, :integer
 
-  attribute :daily_rates, :daily_rate_array, default: []
+  attribute :daily_rates
+  attribute :special_periods
 
   with_options presence: true do
     with_options invalid_words: true do
@@ -24,13 +25,14 @@ class HotelForm
 
   def save
     return if invalid?
-
-    Rails.logger.debug daily_rates.first
+    p normal_period_rates
+    p special_period_rates
 
     ActiveRecord::Base.transaction do
       hotel = Hotel.new(name:, content:, user_id:)
       build_hotel_images(hotel:)
-      build_rest_rates_of_all_days(hotel:)
+
+      build_rest_rates_of_normal_days(hotel:)
       build_special_periods(special_day: build_hotel_days(hotel:, today: '特別期間'))
       hotel.save!
     end
@@ -51,56 +53,40 @@ class HotelForm
     end
 
     # 各曜日ごとに休憩料金をbuild
-    def build_rest_rates_of_all_days(hotel:)
+    def build_rest_rates_of_normal_days(hotel:)
       DAY_OF_WEEK.each do |day|
-        build_friday_rest_rates(day_of_week: build_hotel_days(hotel:, today: day))
+        build_today_rest_rates(day_of_week: build_hotel_days(hotel:, today: day))
       end
     end
 
-    # 休憩料金を
     def build_today_rest_rates(day_of_week:)
-      day_of_week.rest_rates.build(plan: friday_rates[0][:plan], rate: friday_rates[0][:rate], first_time: friday_rates[0][:first_time], last_time: friday_rates[0][:last_time])
+      day_of_week.rest_rates.build(plan: normal_period_rates[0][:plan], rate: normal_period_rates[0][:rate], first_time: normal_period_rates[0][:first_time], last_time: normal_period_rates[0][:last_time])
     end
 
     def build_hotel_days(hotel:, today:)
       hotel.days.build(day: today)
     end
 
-    def rates
-     # daily_rates.dig(:friday, :rest_rates)
-      daily_rates.fetch(:saturday)
-      daily_rates.fetch(:sunday)
-      daily_rates.fetch(:holiday)
+    def build_rest_rates
+      normal_period_rates.map do |each_rates|
+        each_rates
+      end
     end
 
-    # def monday_through_thursday_rates
-    #   daily_rates.fetch(:monday_through_thursday)
-    # end
-
-    def friday_rates
-      daily_rates.fetch(:friday)
+    def normal_period_rates
+      # [{:rest_rates=>[{:plan=>"休憩90分", :rate=>3980, :first_time=>"6:00", :last_time=>"1:00"}], :stay_rates=>[{:plan=>"宿泊1部", :rate=>6980, :first_time=>"6:00", :last_time=>"11:00"}]},
+      #  {:rest_rates=>[{:plan=>"休憩90分", :rate=>3980, :first_time=>"6:00", :last_time=>"1:00"}], :stay_rates=>[{:plan=>"宿泊1部", :rate=>6980, :first_time=>"6:00", :last_time=>"11:00"}]}, 
+      #  {:rest_rates=>[{:plan=>"休憩90分", :rate=>3980, :first_time=>"6:00", :last_time=>"1:00"}], :stay_rates=>[{:plan=>"宿泊1部", :rate=>6980, :first_time=>"6:00", :last_time=>"11:00"}]},
+      #   {:rest_rates=>[{:plan=>"休憩90分", :rate=>3980, :first_time=>"6:00", :last_time=>"1:00"}], :stay_rates=>[{:plan=>"宿泊1部", :rate=>6980, :first_time=>"6:00", :last_time=>"11:00"}]}, 
+      #   {:rest_rates=>[{:plan=>"休憩90分", :rate=>3980, :first_time=>"6:00", :last_time=>"1:00"}], :stay_rates=>[{:plan=>"宿泊1部", :rate=>6980, :first_time=>"6:00", :last_time=>"11:00"}]}]
+      daily_rates.values_at(:monday_through_thursday, :friday, :saturday, :sunday, :holiday)
     end
-
-    # def saturday_rates
-    #   daily_rates.fetch(:saturday)
-    # end
-
-    # def sunday_rates
-    #   daily_rates.fetch(:sunday)
-    # end
-
-    # def holiday_rates
-    #   daily_rates.fetch(:holiday)
-    # end
-
+    
     def special_period_rates
-      special_periods.fetch(:obon)
-      special_periods.fetch(:golden_week)
-      special_periods.fetch(:the_new_years_holiday)
+      special_periods.values_at(:obon, :golden_week, :the_new_years_holiday)
     end
 
     def build_special_periods(special_day:)
-      # {:obon=>[{:period=>1, :start_date=>"8月9日", :end_date=>"8月15日"}], :golden_week=>[{:period=>0, :start_date=>"5月2日", :end_date=>"5月10日"}], :the_new_years_holiday=>[{:period=>2, :start_date=>"12月25日", :end_date=>"1月4日"}]}
       special_day.special_periods.build(period: special_period_rates[:period], start_date: special_period_rates[:start_date], end_date: special_period_rates[:end_date])
     end
 end
