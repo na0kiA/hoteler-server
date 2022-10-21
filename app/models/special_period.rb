f545ed6e66a73f3ea99789cde685b99cda8f7d30
na0kiB <na0ki199823@gmail.com>
2022-10-22 01:03:46 +0900

    [fix]enumの値が複数形になっていたので修正#19

--- app/models/special_period.rb
+++ app/models/special_period.rb
@@ -2,5 +2,5 @@
 
 class SpecialPeriod < ApplicationRecord
   belongs_to :day
-  enum :periods, { golden_week: 0, obon: 1, the_new_years_holiday: 2 }
+  enum :period, { golden_week: 0, obon: 1, the_new_years_holiday: 2 }
 end
