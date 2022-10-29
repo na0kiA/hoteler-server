da466f330ae401571aa05720a8ef589a31efe013
na0kiB <na0ki199823@gmail.com>
2022-10-29 20:04:18 +0900

    [dev]file_urlを生成して返すためにhotel_image_serializerを追加#19

---
+++ app/serializers/hotel_image_serializer.rb
@@ -0,0 +1,15 @@
+# frozen_string_literal: true
+
+class HotelImageSerializer < ActiveModel::Serializer
+  attributes :id, :file_url
+
+  def file_url
+    Aws::S3::Object.new(ENV.fetch('S3_BUCKET', nil), object.key).public_url
+  end
+
+  # private
+
+  # def key
+
+  # end
+end
