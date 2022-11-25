cc5b06c850c7b13d2b19043866858a4ed840b3a3
na0kiB <na0ki199823@gmail.com>
2022-11-26 01:34:11 +0900

    [dev]特別期間当日かどうかを確認するロジックをを追加#23

--- app/models/special_period.rb
+++ app/models/special_period.rb
@@ -9,10 +9,17 @@ class SpecialPeriod < ApplicationRecord
   validates :start_date, presence: true
   validates :end_date, presence: true
 
+
   def self.check_that_today_is_a_special_period?(hotel:)
     start_date_list = hotel.special_periods.pluck(:start_date)
     end_date_list = hotel.special_periods.pluck(:end_date)
 
-    (start_date_list[0]...end_date_list[0]).cover?(Date.current) || (start_date_list[1]...end_date_list[1]).cover?(Date.current) || (start_date_list[2]...end_date_list[2]).cover?(Date.current)
+    (start_date_list[0]..end_date_list[0]).cover?(Date.current) || (start_date_list[1]..end_date_list[1]).cover?(Date.current) || (start_date_list[2]..end_date_list[2]).cover?(Date.current)
+  end
+
+  def self.check_that_today_is_a_last_day_of_special_periods?(hotel:)
+    end_date_list = hotel.special_periods.pluck(:end_date)
+
+    end_date_list[0].eql?(Date.current) || end_date_list[1].eql?(Date.current) || end_date_list[2].eql?(Date.current)
   end
 end
