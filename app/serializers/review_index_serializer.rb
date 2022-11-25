7c0e2ae914f3ecdaa6f78eea32c60cf9cb5acb0c
na0kiB <na0ki199823@gmail.com>
2022-11-25 21:27:32 +0900

    [delete]formオブジェクトをやめたので、関連するモデルを削除#23

--- app/serializers/review_index_serializer.rb
+++ app/serializers/review_index_serializer.rb
@@ -5,18 +5,11 @@ class ReviewIndexSerializer < ActiveModel::Serializer
              :content,
              :five_star_rate,
              :helpfulnesses,
-             :review_images,
              :user_name,
              :created_at
 
   def helpfulnesses
     object.helpfulnesses.length
-  end
-
-  def review_images
-    object.review_images.pluck(:key).map do |val|
-      file_url(key: val)
-    end
   end
 
   def user_name
