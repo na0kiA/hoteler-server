5abec63fd0fb4c4366914bceb777a181eeadb74a
na0kiB <na0ki199823@gmail.com>
2022-11-26 19:42:03 +0900

    [fix]favoritesは主テーブルが大きいのでeager_loadでhotelを左外部結合してキャッシュするように修正#23

--- app/controllers/v1/user_favorites_controller.rb
+++ app/controllers/v1/user_favorites_controller.rb
@@ -4,8 +4,7 @@ class V1::UserFavoritesController < ApplicationController
   before_action :authenticate_v1_user!
 
   def index
-    user = User.find(params[:id])
-    favorites = user.favorites.order(id: :desc)
+    favorites = Favorite.eager_load(:hotel, hotel: [:hotel_images]).where(user_id: params[:id])
     render json: favorites, each_serializer: UserFavoriteSerializer
   end
 end
