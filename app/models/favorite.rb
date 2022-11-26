c19cce15315ec799044875e002d827f50ff40443
na0kiB <na0ki199823@gmail.com>
2022-11-26 21:35:54 +0900

    [fix]favoritesのdefault_scopeを最新順に変更#23

--- app/models/favorite.rb
+++ app/models/favorite.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
 class Favorite < ApplicationRecord
+  default_scope -> { order(created_at: :desc) }
+  
   belongs_to :user
   belongs_to :hotel
   counter_culture :hotel
