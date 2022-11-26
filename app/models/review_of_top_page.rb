fc279ce5946036737b4d0b81b2b6cc8c93005bfb
na0kiB <na0ki199823@gmail.com>
2022-11-26 21:38:05 +0900

    [fix]N1が検出されていたため、eager_loadでreviewにuserを結合#23

--- app/models/review_of_top_page.rb
+++ app/models/review_of_top_page.rb
@@ -8,11 +8,11 @@ class ReviewOfTopPage
   # 参考になったの数が同じ場合は、review_idの新しい順に並べる
 
   def initialize(reviews_of_a_hotel:)
-    @reviews_of_a_hotel = Review.preload(:user, :helpfulnesses).where(id: reviews_of_a_hotel.ids)
+    @reviews_of_a_hotel = Review.where(id: reviews_of_a_hotel.ids)
   end
 
   def extract_top_reviews
-    Review.where(id: sort_by_most_helpful_and_current_review.pluck(0))
+    Review.eager_load(:user).where(id: sort_by_most_helpful_and_current_review.pluck(0))
   end
 
   private
