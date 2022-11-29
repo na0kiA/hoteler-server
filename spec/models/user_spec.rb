c41489848a74ec82332b4d0e2520b8aed2e8d02e
na0kiB <na0ki199823@gmail.com>
2022-11-30 04:21:18 +0900

    [fix]不必要なuser_specを削除#25

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,13 +3,5 @@
 require "rails_helper"
 
 RSpec.describe User, type: :model do
-  describe "has_many #hotels" do
-    let_it_be(:user) { create(:user) }
-    context "userを削除した場合" do
-      it "userを削除したときuserが投稿したhotelsは削除されること" do
-        create(:accepted_hotel, user:)
-        expect { user.destroy }.to change(Hotel, :count).by(-1)
-      end
-    end
-  end
+  pending "add some examples to (or delete) #{__FILE__}"
 end
