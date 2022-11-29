90c5f5493a2cbd0318750bf9363660808ebd71d2
na0kiB <na0ki199823@gmail.com>
2022-11-30 04:20:12 +0900

    [dev]sessionが有効か無効かを確認するspecを追加#25

--- spec/requests/v1/auth/sessions_spec.rb
+++ spec/requests/v1/auth/sessions_spec.rb
@@ -13,7 +13,49 @@ RSpec.describe "V1::Auth::SessionsController", type: :request do
           email: client_user.email,
           password: client_user.password
         }
-        expect(response).to have_http_status(:unauthorized)
+        expect(response.status).to eq(401)
+      end
+    end
+  end
+
+  describe "describe DELETE /v1/auth/sign_out - v1/auth/sessions#destroy" do
+    context "サインアウトができる場合" do
+      let_it_be(:client_user) { create(:user) }
+      let_it_be(:auth_tokens) { client_user.create_new_auth_token }
+
+      it "200を返すこと" do
+        delete destroy_v1_user_session_path, headers: auth_tokens
+        expect(response.status).to eq(200)
+      end
+    end
+
+    context "サインアウトができない場合" do
+      let_it_be(:client_user) { create(:user) }
+      let_it_be(:auth_tokens) { client_user.create_new_auth_token }
+
+      it "200を返すこと" do
+        delete destroy_v1_user_session_path
+        expect(response.status).to eq(404)
+        expect(symbolized_body(response)[:errors][0]).to eq("ユーザーが見つからないか、ログインしていません。")
+      end
+    end
+  end
+
+  describe "describe GET /v1/auth/sessions - v1/auth/sessions#index" do
+    context "ログインしている場合" do
+      let_it_be(:client_user) { create(:user) }
+      let_it_be(:auth_tokens) { client_user.create_new_auth_token }
+
+      it "is_loginがtrueを返すこと" do
+        get v1_auth_sessions_path, headers: auth_tokens
+        expect(symbolized_body(response)[:is_login]).to eq(true)
+      end
+    end
+
+    context "ログインしていない場合" do
+      it "is_loginがfalseを返すこと" do
+        get v1_auth_sessions_path
+        expect(symbolized_body(response)[:is_login]).to eq(false)
       end
     end
   end
