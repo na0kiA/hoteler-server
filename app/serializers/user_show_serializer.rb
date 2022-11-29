691652522833b9eab89d60a39450870b294c2ea0
na0kiB <na0ki199823@gmail.com>
2022-11-30 02:39:31 +0900

    [dev]userのreviewの詳細とホテルに表示するreview一覧は違うためshowのシリアライザを追加#25

--- app/serializers/user_show_serializer.rb
+++ app/serializers/user_show_serializer.rb
@@ -10,19 +10,14 @@ class UserShowSerializer < ActiveModel::Serializer
     return "口コミはまだありません。" if object.reviews.blank?
 
     ActiveModelSerializers::SerializableResource.new(
-      object.reviews,
-      each_serializer: ReviewIndexSerializer,
-      user_image: object.image,
+      object.reviews.eager_load(:hotel, hotel: [:hotel_images]),
+      each_serializer: ReviewShowSerializer,
       adapter: :attributes
     ).serializable_hash
   end
 
   def image
-    if object.image.blank?
-      "blank-profile-picture-g89cfeb4dc_640.png"
-    else
-      file_url
-    end
+    file_url
   end
 
   private
@@ -30,5 +25,4 @@ class UserShowSerializer < ActiveModel::Serializer
     def file_url
       Aws::S3::Object.new(ENV.fetch("S3_BUCKET", nil), object.image).public_url
     end
-
 end
