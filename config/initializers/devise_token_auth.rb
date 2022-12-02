991e46b692d1306bd9ab3801f51dff4141e4600e
na0kiB <na0ki199823@gmail.com>
2022-12-02 23:48:58 +0900

    [fix]email編集時に確認のメールが行くように変更#25

--- config/initializers/devise_token_auth.rb
+++ config/initializers/devise_token_auth.rb
@@ -57,4 +57,6 @@ DeviseTokenAuth.setup do |config|
   # devise confirmable module. If you want to use devise confirmable module and
   # send email, set it to true. (This is a setting for compatibility)
   config.send_confirmation_email = true
+  config.default_confirm_success_url = "http://localhost:3001/"
+  config.default_password_reset_url = "http://localhost:3001/"
 end
