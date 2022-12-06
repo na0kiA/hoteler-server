b4f8851096a4c8d15c776d66c8aedeb7ff608b4b
na0kiB <na0ki199823@gmail.com>
2022-12-06 18:38:50 +0900

    [dev]パラメーターの最初が空欄の場合全ての候補が追加されてしまうのでselectで存在しているものだけを抽出28

--- app/controllers/v1/search_controller.rb
+++ app/controllers/v1/search_controller.rb
@@ -1,37 +1,66 @@
-class V1::SearchController < ApplicationController
+# frozen_string_literal: true
 
+class V1::SearchController < ApplicationController
   def index
     @accepted_hotel = Hotel.accepted
+    @hotel = Hotel.none
 
     if search_params[:keyword].present?
-      split_keyword.each do |keyword|
-        @hotel = []
-        @hotel << @accepted_hotel.search_multiple(keyword)
+      search_each_params_of_keyword(split_params: split_keyword)
+      if @hotel.blank?
+        render json: render_not_match_params(search_params[:keyword])
+      else
+        render json: @hotel, each_serializer: HotelIndexSerializer
       end
-      render json: @hotel.first, each_serializer: HotelIndexSerializer
-    end
 
-    if search_params[:city].present?
-      split_keyword.each do |keyword|
-        @hotel = []
-        @hotel << @accepted_hotel.search_multiple(keyword)
+    elsif search_params[:city_and_street_address].present?
+      search_each_params_of_city_or_street_address(split_params: split_city_and_street_address)
+      if @hotel.blank?
+        render json: render_not_match_params(search_params[:city_and_street_address])
+      else
+        render json: @hotel, each_serializer: HotelIndexSerializer
       end
-      render json: @hotel.first, each_serializer: HotelIndexSerializer
-    end
 
-    if search_params.blank?
+    else
       redirect_to v1_hotels_path
     end
-
   end
 
   private
 
-  def split_keyword
-    search_params[:keyword].split(/[[:blank:]]+/)
-  end
+    def search_each_params_of_city_or_street_address(split_params:)
+      split_params.each do |city_and_street_address|
+        @hotel = @hotel.or(search_city_and_street_address(city_and_street_address:))
+      end
+    end
 
-  def search_params
-    params.permit(:name, :prefecture, :city, :street_address, :postal_code, :company, :keyword)
-  end
+    def search_each_params_of_keyword(split_params:)
+      split_params.each do |keyword|
+        @hotel = @hotel.or(search_keyword(keyword:))
+      end
+    end
+
+    def search_keyword(keyword:)
+      @accepted_hotel.search_multiple(keyword)
+    end
+
+    def search_city_and_street_address(city_and_street_address:)
+      @accepted_hotel.search_city_and_street_address(city_and_street_address)
+    end
+
+    def render_not_match_params(params)
+      "#{params}の検索結果が見つかりませんでした"
+    end
+
+    def split_keyword
+      search_params[:keyword].split(/[[:blank:]]+/).select(&:present?)
+    end
+
+    def split_city_and_street_address
+      search_params[:city_and_street_address].split(/[[:blank:]]+/).select(&:present?)
+    end
+
+    def search_params
+      params.permit(:keyword, :city_and_street_address)
+    end
 end
--- spec/requests/v1/search_spec.rb
+++ spec/requests/v1/search_spec.rb
@@ -1,42 +1,82 @@
-require 'rails_helper'
+# frozen_string_literal: true
+
+require "rails_helper"
 
 RSpec.describe "V1::Searches", type: :request do
   describe "GET /v1/search/ - v1/search#index" do
     let_it_be(:hotel) { create(:accepted_hotel, user: create(:user)) }
 
-    context '検索用のパラメーターに何も付与されていない場合' do
-      it 'トップページを返すこと' do
+    context "キーワードに何も付与されていない場合" do
+      it "トップページを返すこと" do
         get v1_search_index_path
         expect(response.status).to eq(302)
         expect(response.body).to include("redirected")
       end
     end
 
-    context '検索用のパラメーターにホテルの項目と一致する文字が入力された場合' do
-      it '文字に一致するホテルを返す' do
-        get v1_search_index_path, params: {keyword: "hotel"}
+    context "キーワードにホテルの項目と一致する文字が入力された場合" do
+      it "文字に一致するホテルを返すこと" do
+        get v1_search_index_path, params: { keyword: "hotel" }
         expect(symbolized_body(response)[0][:name]).to eq(hotel.name)
         expect(symbolized_body(response).first.length).to eq(13)
       end
 
-      it '文字に一致する都市のホテルを返す' do
-        get v1_search_index_path, params: {keyword: "渋谷"}
+      it "文字に一致する都市のホテルを返すこと" do
+        get v1_search_index_path, params: { keyword: "渋谷" }
         expect(symbolized_body(response).first.length).to eq(13)
       end
     end
 
-    context '検索用のパラメーターに複数個の値が入力された場合' do
-      it '文字に一致するホテルを返す' do
-        get v1_search_index_path, params: {keyword: "にゃあ 渋谷"}
-        p symbolized_body(response)
+    context "キーワードに複数個の値が入力された場合" do
+      it "文字に一致するホテルを返すこと" do
+        get v1_search_index_path, params: { keyword: "にゃあ　渋谷" }
         expect(symbolized_body(response).first.length).to eq(13)
       end
     end
 
-    context '検索用のパラメーターにどのホテルのカラムとも一致しない文字が入力された場合' do
-      it '検索結果に何も表示されないこと' do
-        get v1_search_index_path, params: {keyword: "にゃあ"}
-        expect(symbolized_body(response)).to eq([])
+    context "キーワードにどのホテルのカラムとも一致しない文字が入力された場合" do
+      it "検索結果に何も表示されないこと" do
+        get v1_search_index_path, params: { keyword: "にゃあ" }
+        expect(response.body).to eq("にゃあの検索結果が見つかりませんでした")
+      end
+    end
+
+    context "キーワードが空欄の場合" do
+      it "検索結果に何も表示されないこと" do
+        get v1_search_index_path, params: { keyword: "　" }
+        expect(response.status).to eq(302)
+        expect(response.body).to include("redirected")
+      end
+    end
+
+    context "キーワードの最初が空欄かつ存在しない語句の場合" do
+      it "検索結果に何も表示されないこと" do
+        get v1_search_index_path, params: { keyword: " にゃあ" }
+        expect(response.body).to eq(" にゃあの検索結果が見つかりませんでした")
+      end
+    end
+
+    context "市区町村に存在するホテルの住所のパラメーターに与えられた場合" do
+      it "該当するホテルが検索されること" do
+        get v1_search_index_path, params: { city_and_street_address: "丁目" }
+        expect(symbolized_body(response).first.length).to eq(13)
+      end
+
+      it "パラメーターが5つでも該当するホテルが検索されること" do
+        get v1_search_index_path, params: { city_and_street_address: "丁目　にゃあ　わん　やあ　ほげ" }
+        expect(symbolized_body(response).first.length).to eq(13)
+      end
+
+      it "パラメーターが7つでも該当するホテルが検索されること" do
+        get v1_search_index_path, params: { city_and_street_address: "にゃあ　わん　やあ　ほげ　ふー　ばず　渋谷" }
+        expect(symbolized_body(response).first.length).to eq(13)
+      end
+    end
+
+    context "市区町村に存在しないホテルの住所のパラメーターに与えられた場合" do
+      it "該当するホテルが検索されないこと" do
+        get v1_search_index_path, params: { city_and_street_address: "にゃあ" }
+        expect(response.body).to eq("にゃあの検索結果が見つかりませんでした")
       end
     end
   end
