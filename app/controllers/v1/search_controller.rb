fafea769f5a398008846d523f47f9cb9c5e07a1f
na0kiB <na0ki199823@gmail.com>
2022-12-10 15:40:40 +0900

    [fix]早期リターンで条件分岐の簡略化、正常系か異常系かでメソッドを抽出#28

--- app/controllers/v1/search_controller.rb
+++ app/controllers/v1/search_controller.rb
@@ -2,33 +2,49 @@
 
 class V1::SearchController < ApplicationController
   def index
-    @accepted_hotel = Hotel.accepted
-    @hotel = Hotel.none
-
     if search_params[:keyword].present?
-      search_each_params_of_keyword(split_params: split_keyword)
-      hotels = HotelSort.new(hotels: @hotel)
-      if sort_by_low_rest?
-        render json: hotels.sort_by_low_rest, each_serializer: HotelIndexSerializer
-      elsif sort_by_high_rest?
-        render json: hotels.sort_by_high_rest, each_serializer: HotelIndexSerializer
-      elsif sort_by_low_stay?
-        render json: hotels.sort_by_low_stay, each_serializer: HotelIndexSerializer
-      elsif sort_by_high_stay?
-        render json: hotels.sort_by_high_stay, each_serializer: HotelIndexSerializer
-      elsif @hotel.blank?
-        render json: render_not_match_params(search_params[:keyword])
-      else
-        render json: @hotel, each_serializer: HotelIndexSerializer
-      end
-    end
-
-    if search_params.blank? || (search_params[:keyword].blank? && search_params[:sort].blank?)
+      sort_or_not
+    else
       redirect_to v1_hotels_path
     end
   end
 
   private
+
+    def sort_or_not
+      searched_hotel_list = search_each_params_of_keyword(box_for_searched_list: Hotel.none, split_params: split_keyword, accepted_hotel: Hotel.accepted)
+      hotels = HotelSort.new(hotels: searched_hotel_list)
+
+      should_sort(hotels:)
+      no_sort(searched_hotel_list:)
+    end
+
+    def should_sort(hotels:)
+      return render json: hotels.sort_by_high_rest, each_serializer: HotelIndexSerializer if sort_by_high_rest?
+      return render json: hotels.sort_by_low_rest, each_serializer: HotelIndexSerializer if sort_by_low_rest?
+      return render json: hotels.sort_by_low_stay, each_serializer: HotelIndexSerializer if sort_by_low_stay?
+      return render json: hotels.sort_by_high_stay, each_serializer: HotelIndexSerializer if sort_by_high_stay?
+    end
+
+    def no_sort(searched_hotel_list:)
+      return render json: render_not_match_params(search_params[:keyword]), each_serializer: HotelIndexSerializer if searched_hotel_list.blank?
+      return render json: searched_hotel_list, each_serializer: HotelIndexSerializer if search_params[:sort].blank?
+    end
+
+    def search_each_params_of_keyword(box_for_searched_list:, split_params:, accepted_hotel:)
+      split_params.each do |keyword|
+        box_for_searched_list = box_for_searched_list.or(search_keyword(keyword:, accepted_hotel:))
+      end
+      box_for_searched_list
+    end
+
+    def search_keyword(keyword:, accepted_hotel:)
+      accepted_hotel.search_multiple(keyword)
+    end
+
+    def render_not_match_params(params)
+      "#{params}の検索結果が見つかりませんでした"
+    end
 
     def sort_by_low_rest?
       search_params[:sort] == "low_rest"
@@ -44,20 +60,6 @@ class V1::SearchController < ApplicationController
 
     def sort_by_high_stay?
       search_params[:sort] == "high_stay"
-    end
-
-    def search_each_params_of_keyword(split_params:)
-      split_params.each do |keyword|
-        @hotel = @hotel.or(search_keyword(keyword:))
-      end
-    end
-
-    def search_keyword(keyword:)
-      @accepted_hotel.search_multiple(keyword)
-    end
-
-    def render_not_match_params(params)
-      "#{params}の検索結果が見つかりませんでした"
     end
 
     def split_keyword
