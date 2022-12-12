041182670aa98e95cd52535bee77fca051454825
na0kiB <na0ki199823@gmail.com>
2022-12-12 19:32:54 +0900

    [dev]ホテル設備を利用可能に更新できるようにロジックを追加#28

--- config/routes.rb
+++ config/routes.rb
@@ -15,6 +15,7 @@ Rails.application.routes.draw do
         resources :reviews, shallow: true
         resources :images, only: %i[index show create], controller: "hotel_images"
         resources :days, only: %i[index]
+        resource :hotel_facilities, only: %i[update]
         resource :favorites, only: %i[create destroy]
       end
     end
---
+++ spec/requests/v1/hotel_facilities_spec.rb
@@ -0,0 +1,21 @@
+require 'rails_helper'
+
+RSpec.describe "V1::HotelFacilities", type: :request do
+  describe "PATCH /v1/hotels/:hotel_id/hotel_facilities - v1/hotel_facilities #update" do
+    context 'ホテルの設備を更新する場合' do
+      let_it_be(:client_user) { create(:user) }
+      let_it_be(:auth_tokens) { client_user.create_new_auth_token }
+      let_it_be(:hotel) { create(:hotel, user: client_user) }
+
+      it 'wifiを利用可能にできること' do
+        patch v1_hotel_hotel_facilities_path(hotel.id), params: {hotel_facilities: {wifi_enabled: true}}, headers: auth_tokens
+        expect(symbolized_body(response)[:hotel_facilities][:wifi_enabled]).to eq("Wi-Fiあり")
+      end
+
+      it '全ての設備を利用可能にできること' do
+        patch v1_hotel_hotel_facilities_path(hotel.id), params: {hotel_facilities: {wifi_enabled: true, parking_enabled: true, triple_rooms_enabled: true, secret_payment_enabled: true, credit_card_enabled: true, phone_reservation_enabled: true, net_reservation_enabled: true, cooking_enabled: true, breakfast_enabled: true}}, headers: auth_tokens
+        expect(symbolized_body(response)[:hotel_facilities][:breakfast_enabled]).to eq("朝食あり")
+      end
+    end
+  end
+end
