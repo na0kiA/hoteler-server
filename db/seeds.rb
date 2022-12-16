70ea1998ee44d56515f39ef66e3dc700caf367b2
na0kiA <na0ki199823@gmail.com>
2022-12-17 00:15:10 +0900

    [dev]faker gem を用いてダミーデータを挿入#32

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,8 +1,30 @@
-10.times do |n|
-  Hotel.create!(
-    name: "ホテル#{n + 1}",
-    content: "ホテル情報#{n + 1}",
-    user_id: 1,
-    accepted: false
+include FactoryBot::Syntax::Methods
+
+  hotel = Hotel.create_list(
+    :completed_profile_hotel,
+    :with_user,
+    :with_service_rates,
+    :with
+    user: User.fitst,
+    name: Faker::JapaneseMedia::StudioGhibli.unique.character,
+    content: "当ホテルでは",
+    accepted: true,
+    created_at: Faker::Time.between(from: DateTime.now - 1, to: DateTime.now, format: :default),
+    updated_at: Faker::Time.between(from: DateTime.now - 1, to: DateTime.now, format: :default),
+    favorites_count: Faker::Number.number(digits: 2),
+    full: Faker::Boolean.boolean(true_ratio: 0.2),
+    prefecture: Faker::Address.state,
+    city: Faker::Address.city,
+    postal_code: Faker::Address.postcode,
+    street_address: Faker::Address.street_address,
+    phone_number: Faker::PhoneNumber.phone_number,
+    company: Faker::Name.first_name + Faker::Company.suffix
   )
+
+  hotel.hotel_images.key
+
+  (1..5).to_a.sample.times do
+    Review.create(hotel_id: hotel.id, rating: (1..5).to_a.sample, title: Faker::Lorem.word, body: Faker::Lorem.paragraph, user: User.all.sample)
+  end
+
 end
