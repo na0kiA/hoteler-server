633776699b952eb87469278ae194c67b55e68a64
na0kiA <na0ki199823@gmail.com>
2022-12-17 22:36:48 +0900

    [fix]住所の日本語がおかしかったので番地以下を修正

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,13 +1,12 @@
 include FactoryBot::Syntax::Methods
 
-  hotel = Hotel.create_list(
-    :completed_profile_hotel,
+2.times do |i|
+  hotel = create(
+    :with_service_completed_hotel,
     :with_user,
-    :with_service_rates,
-    :with
-    user: User.fitst,
     name: Faker::JapaneseMedia::StudioGhibli.unique.character,
-    content: "当ホテルでは",
+    content: "【お客様各位】
+    当ホテルへのご来店誠にありがとうございます。",
     accepted: true,
     created_at: Faker::Time.between(from: DateTime.now - 1, to: DateTime.now, format: :default),
     updated_at: Faker::Time.between(from: DateTime.now - 1, to: DateTime.now, format: :default),
@@ -16,15 +15,18 @@ include FactoryBot::Syntax::Methods
     prefecture: Faker::Address.state,
     city: Faker::Address.city,
     postal_code: Faker::Address.postcode,
-    street_address: Faker::Address.street_address,
+    street_address: "#{(1..9).to_a.sample}の#{(11..50).to_a.sample}",
     phone_number: Faker::PhoneNumber.phone_number,
     company: Faker::Name.first_name + Faker::Company.suffix
   )
 
-  hotel.hotel_images.key
-
-  (1..5).to_a.sample.times do
-    Review.create(hotel_id: hotel.id, rating: (1..5).to_a.sample, title: Faker::Lorem.word, body: Faker::Lorem.paragraph, user: User.all.sample)
+  (1..2).to_a.sample.times do
+    create(:review, hotel_id: hotel.id, five_star_rate: (1..2).to_a.sample, title: "正直あまり良くなかった", content: "値段の割にはそこまででした。", user_id: create(:other_user).id)
   end
-
+  (2..3).to_a.sample.times do
+    create(:review, hotel_id: hotel.id, five_star_rate: (3..4).to_a.sample, title: "普通に良かったです", content: "部屋が綺麗でした。また利用しようと思っています。", user_id: create(:other_user2).id)
+  end
+  (2..6).to_a.sample.times do
+    create(:review, hotel_id: hotel.id, five_star_rate: 5, title: "最高でした", content: "フロントの対応もよくて、アメニティも豊富で良かったです。", user_id: create(:other_user3).id)
+  end
 end
