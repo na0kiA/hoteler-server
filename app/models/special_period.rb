5c9e1d3de1bf08f592e5308e9ed7b8a10b9a8b36
na0kiA <na0ki199823@gmail.com>
2022-12-18 01:09:32 +0900

    [fix]二度発行していたpluckクエリを解消するために一つにまとめた

--- app/models/special_period.rb
+++ app/models/special_period.rb
@@ -10,10 +10,8 @@ class SpecialPeriod < ApplicationRecord
   validates :end_date, presence: true
 
   def self.check_that_today_is_a_special_period?(hotel:)
-    start_date_list = hotel.special_periods.pluck(:start_date)
-    end_date_list = hotel.special_periods.pluck(:end_date)
-
-    (start_date_list[0]..end_date_list[0]).cover?(Date.current) || (start_date_list[1]..end_date_list[1]).cover?(Date.current) || (start_date_list[2]..end_date_list[2]).cover?(Date.current)
+    special_weeks = hotel.special_periods.pluck(:start_date, :end_date).flatten
+    (special_weeks[0]..special_weeks[1]).cover?(Date.current) || (special_weeks[2]..special_weeks[3]).cover?(Date.current) || (special_weeks[4]..special_weeks[5]).cover?(Date.current)
   end
 
   def self.check_that_today_is_a_last_day_of_special_periods?(hotel:)
