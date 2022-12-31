2e09b456e3f2e2a4d8828347130f075a5c24c379
na0kiA <na0ki199823@gmail.com>
2022-12-31 12:10:16 +0900

    [delete]不必要なディレクトリをファイルを削除#34

--- app/controllers/posts_controller.rb
+++
@@ -1,8 +0,0 @@
-# frozen_string_literal: true
-
-class PostsController < ApiController
-  before_action :authenticate_v1_user!, except: %i[index]
-  def index
-    render json: { posts: Post.limit(50) }
-  end
-end
