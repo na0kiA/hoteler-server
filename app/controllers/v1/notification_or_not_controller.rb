2e05359976a4d03eb3d545e07d0a58834b9a91ac
na0kiA <na0ki199823@gmail.com>
2023-02-28 01:37:34 +0900

    [dev]通知があるかどうかのリソースの追加#34

---
+++ app/controllers/v1/notification_or_not_controller.rb
@@ -0,0 +1,15 @@
+# frozen_string_literal: true
+
+class V1::NotificationsController < ApplicationController
+  before_action :authenticate_v1_user!
+
+  def index
+    notifications = current_v1_user.notifications
+    if notifications.blank?
+      render json: []
+    else
+      not_read_notification = notifications.where(read: false).count
+      render json: not_read_notification
+    end
+  end
+end
