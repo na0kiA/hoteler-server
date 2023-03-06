323f4ecc443b429cb314db19ca5aa518902bb15e
na0kiA <na0ki199823@gmail.com>
2023-03-07 02:59:08 +0900

    [dev]networkを追加#38

---
+++ terraform/network/main/variables.tf
@@ -0,0 +1,21 @@
+variable "vpc_cidr" {
+  type    = string
+  default = "171.32.0.0/16"
+}
+
+variable "azs" {
+  type = map(object({
+    public_cidr  = string
+    private_cidr = string
+  }))
+  default = {
+    a = {
+      public_cidr  = "171.32.0.0/20"
+      private_cidr = "171.32.48.0/20"
+    },
+    c = {
+      public_cidr  = "171.32.16.0/20"
+      private_cidr = "171.32.64.0/20"
+    }
+  }
+}
