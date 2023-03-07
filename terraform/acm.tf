22409bd4c7281a29e02edd457dedb5f28796b0a2
na0kiA <na0ki199823@gmail.com>
2023-03-07 20:07:39 +0900

    [dev]acmとroute53に証明書発行のリソースを追加#38

---
+++ terraform/acm.tf
@@ -0,0 +1,18 @@
+resource "aws_acm_certificate" "root" {
+  domain_name = data.aws_route53_zone.this.name
+
+  validation_method = "DNS"
+
+  tags = {
+    Name = "${local.service_name}-lovehoteler-com"
+  }
+
+  # デフォルトではリソースが削除されてから作成されるが、証明書に関してはロードバランサーリスナーで使用中なのを考慮して、作成してから削除させる
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+resource "aws_acm_certificate_validation" "root" {
+  certificate_arn = aws_acm_certificate.root.arn
+}
