17fa0c5e95e18bc805aaf1d336e157092c1fc9b4
na0kiA <na0ki199823@gmail.com>
2023-03-10 17:53:21 +0900

    [fix]rubocopに沿って修正#38

--- config/puma.rb
+++ config/puma.rb
@@ -21,11 +21,9 @@ worker_timeout 3600 if ENV.fetch("RAILS_ENV", "development") == "development"
 # environment ENV.fetch("RAILS_ENV", "development")
 environment ENV.fetch("RAILS_ENV", "production")
 
-
 app_root = File.expand_path("..", __dir__)
 bind "unix://#{app_root}/tmp/sockets/puma.sock"
 stdout_redirect "#{app_root}/log/puma.stdout.log", "#{app_root}/log/puma.stderr.log", true
-
 
 # Specifies the `pidfile` that Puma will use.
 pidfile ENV.fetch("PIDFILE", "tmp/pids/server.pid")
