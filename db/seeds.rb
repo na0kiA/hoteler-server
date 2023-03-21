a733d90db2ec7d3bc298add04244aa2499e6efd6
na0kiA <na0ki199823@gmail.com>
2023-03-22 02:11:00 +0900

    [dev]サンプルデータのseedsと挿入用の配列を作成#38

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,7 +6,7 @@ include FactoryBot::Syntax::Methods
 one_to_nine = (1..9).to_a.sample
 
 
-10.times do |i|
+30.times do |i|
   hotel = create(
     :with_service_completed_hotel,
     :with_user,
@@ -33,21 +33,18 @@ one_to_nine = (1..9).to_a.sample
     create(:hotel_image , hotel_id: hotel.id, key: "uploads/hoteler/4786f605-a290-4849-929f-cafbacb46beb/hotel-top-#{(0..119).to_a.sample}.jpg")
   end
 
-  other_user = create_list(:other_user, 20)
-  p other_user
-  p other_user.sample
-  other_user1 = create_list(:other_user, 50)
-  other_user2 = create_list(:other_user, 80)
-
   (1..2).to_a.sample.times do
-    create(:review, hotel_id: hotel.id, five_star_rate: (1..2).to_a.sample, title: one_or_two_reviews_title.sample, content: two_or_one_reviews_content.sample, user: other_user.sample )
+    sample_user = create(:sample_user, name: user_name.sample)
+    create(:review, hotel_id: hotel.id, five_star_rate: (1..2).to_a.sample, title: one_or_two_reviews_title.sample, content: two_or_one_reviews_content.sample, user: sample_user )
   end
 
   (2..3).to_a.sample.times do
-    create(:review, hotel_id: hotel.id, five_star_rate: 3, title: three_reviews_title.sample, content: three_reviews_content.sample, user: other_user1.sample )
+    sample_user = create(:sample_user, name: user_name.sample)
+    create(:review, hotel_id: hotel.id, five_star_rate: 3, title: three_reviews_title.sample, content: three_reviews_content.sample, user: sample_user)
   end
 
   (3..9).to_a.sample.times do
-    create(:review, hotel_id: hotel.id, five_star_rate: (4..5).to_a.sample, title: five_or_four_reviews_title.sample, content: five_or_four_reviews_content.sample, user: other_user2.sample)
+    sample_user = create(:sample_user, name: user_name.sample)
+    create(:review, hotel_id: hotel.id, five_star_rate: (4..5).to_a.sample, title: five_or_four_reviews_title.sample, content: five_or_four_reviews_content.sample, user: sample_user)
   end
 end
