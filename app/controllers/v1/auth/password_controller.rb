8691ef4c7682ee5b24a1e1afa6900f120aa43f31
na0kiA <na0ki199823@gmail.com>
2023-03-24 15:28:32 +0900

    dev]ゲストユーザーの削除と情報の変更ができないように開発#38

--- app/controllers/v1/auth/password_controller.rb
+++ app/controllers/v1/auth/password_controller.rb
@@ -1,4 +1,11 @@
 # frozen_string_literal: true
 
 class V1::Auth::PasswordController < ApplicationController
+  before_action :prohibit_chages_to_guest_use, only: %i[update create edit]
+
+  private
+
+  def prohibit_chages_to_guest_user
+    render_json_bad_request_with_custom_errors("変更できません。", "ゲストユーザーのパスワードは変更できません。") if current_v1_user.uid == "iam_guest_user@eripo.net"
+  end
 end
--- app/controllers/v1/auth/registrations_controller.rb
+++ app/controllers/v1/auth/registrations_controller.rb
@@ -3,10 +3,16 @@
 module V1
   module Auth
     class RegistrationsController < DeviseTokenAuth::RegistrationsController
+      before_action :prohibit_chages_to_guest_use, only: %i[update destroy]
+
       private
 
         def sign_up_params
-          params.permit(:name, :email, :password, :password_confirmation)
+          params.permit(:name, :email, :image, :password, :password_confirmation)
+        end
+
+        def prohibit_chages_to_guest_user
+          render_json_bad_request_with_custom_errors("変更できません。", "ゲストユーザーの情報は変更できません。") if current_v1_user.uid == "iam_guest_user@eripo.net"
         end
 
         def account_update_params
