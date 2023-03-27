d4f7907bebdace6912bba485c73db8a5d1bfc316
na0kiA <na0ki199823@gmail.com>
2023-03-27 17:35:08 +0900

    [dev]サンプルデータ挿入のためのfactory追加#38

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,11 +11,7 @@ one_to_nine = (1..9).to_a.sample
     :with_random_service_completed_hotel,
     :with_user,
     name: hotel_names.sample,
-    content: "
-#{hotel_content.sample}
-
-#{hotel_fee.sample}
-",
+    content: "#{hotel_content.sample}#{hotel_fee.sample}",
     accepted: true,
     created_at: Faker::Time.between(from: DateTime.now - 1, to: DateTime.now, format: :default),
     updated_at: Faker::Time.between(from: DateTime.now - 1, to: DateTime.now, format: :default),
@@ -33,19 +29,17 @@ one_to_nine = (1..9).to_a.sample
     create(:hotel_image , hotel_id: hotel.id, key: "uploads/hoteler/4786f605-a290-4849-929f-cafbacb46beb/hotel-top-#{(0..119).to_a.sample}.jpg")
   end
 
-    create(:hotel_facility, hotel_id: hotel.id, wifi_enabled: Faker::Boolean.boolean(true_ratio: 0.5), parking_enabled: Faker::Boolean.boolean(true_ratio: 0.5),  breakfast_enabled: Faker::Boolean.boolean(true_ratio: 0.5), credit_card_enabled: Faker::Boolean.boolean(true_ratio: 0.5), phone_reservation_enabled: Faker::Boolean.boolean(true_ratio: 0.5), net_reservation_enabled: Faker::Boolean.boolean(true_ratio: 0.5), triple_rooms_enabled: Faker::Boolean.boolean(true_ratio: 0.5), secret_payment_enabled: Faker::Boolean.boolean(true_ratio: 0.5), cooking_enabled: Faker::Boolean.boolean(true_ratio: 0.5), coupon_enabled: Faker::Boolean.boolean(true_ratio: 0.5))
-
   (1..4).to_a.sample.times do
     sample_user = create(:sample_user, name: user_name.sample)
     create(:review, hotel_id: hotel.id, five_star_rate: (1..2).to_a.sample, title: one_or_two_reviews_title.sample, content: two_or_one_reviews_content.sample, user: sample_user )
   end
 
-  (3..9).to_a.sample.times do
+  (2..6).to_a.sample.times do
     sample_user = create(:sample_user, name: user_name.sample)
     create(:review, hotel_id: hotel.id, five_star_rate: 3, title: three_reviews_title.sample, content: three_reviews_content.sample, user: sample_user)
   end
 
-  (5..20).to_a.sample.times do
+  (3..20).to_a.sample.times do
     sample_user = create(:sample_user, name: user_name.sample)
     create(:review, hotel_id: hotel.id, five_star_rate: (4..5).to_a.sample, title: five_or_four_reviews_title.sample, content: five_or_four_reviews_content.sample, user: sample_user)
   end
