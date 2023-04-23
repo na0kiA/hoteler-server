bddddb8b5eba2d048c5033fc74d402c5c74429b2
na0kiA <na0ki199823@gmail.com>
2023-04-23 14:36:35 +0900

    [dev]部屋の状況と料金の状況を確認できるコントローラーの作成

---
+++ app/controllers/v1/price_controller.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+class V1::PriceController < ApplicationController
+  # def index
+  #   price = Price.where(hotel_id: params[:hotel_id])
+  #   render json: price, each_serializer: PriceSerializer
+  # end
+end
---
+++ app/controllers/v1/room_status_controller.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+class V1::RoomStatusController < ApplicationController
+  # def index
+  #   room_status = RoomStatus.where(hotel_id: params[:hotel_id])
+  #   render json: room_status, each_serializer: RoomStatusSerializer
+  # end
+end
