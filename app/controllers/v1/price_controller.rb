61e63c227111cbbaad57b6a9c929d08f5944e63e
na0kiA <na0ki199823@gmail.com>
2023-04-24 16:56:56 +0900

    [dev]料金の幅からホテルを抽出するindexアクションを追加

--- app/controllers/v1/price_controller.rb
+++ app/controllers/v1/price_controller.rb
@@ -1,8 +1,20 @@
 # frozen_string_literal: true
 
 class V1::PriceController < ApplicationController
-  # def index
-  #   price = Price.where(hotel_id: params[:hotel_id])
-  #   render json: price, each_serializer: PriceSerializer
-  # end
+  def index
+    if params[:service_type] == "休憩"
+      search_rest_rate = RestRate.where(rate: price_params[:min_price]..price_params[:max_price])
+      hotels = Hotel.rest_rates.where(id: search_rest_rate.pluck(:hotel_id))
+    else
+      search_stay_rate = StayRate.where(rate: price_params[:min_price]..price_params[:max_price])
+      hotels = Hotel.rest_rates.where(id: search_stay_rate.pluck(:hotel_id))
+    end
+    render json: hotels, each_serializer: HotelIndexSerializer, services: ExtractTodayService.new(hotels:).extract_today_services
+  end
+
+  private
+
+    def price_params
+      params.permit(:min_price, :max_price, :service_type)
+    end
 end
