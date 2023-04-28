79fd9e6d6a0e8f1b0406769b6caf8b0e664e0866
na0kiA <na0ki199823@gmail.com>
2023-04-28 17:32:47 +0900

    [dev]空室状況の検索ができるように開発

--- app/controllers/v1/room_status_controller.rb
+++ app/controllers/v1/room_status_controller.rb
@@ -2,6 +2,7 @@
 
 class V1::RoomStatusController < ApplicationController
   def index
-    
+    hotel = Hotel.where(full: false)
+    render json: hotel, each_serializer: HotelIndexSerializer
   end
 end
