8cbad414cc4b165ac2e919e0942097ab726fb563
na0kiA <na0ki199823@gmail.com>
2023-04-29 18:24:31 +0900

    [add]空室状況を確認するためのrequest specを追加

---
+++ spec/requests/v1/room_status_spec.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+require "rails_helper"
+
+RSpec.describe "V1::RoomStatus", type: :request do
+  # describe "GET /v1/search/room_status - v1_search_room_status#index" do
+
+  # end
+end
