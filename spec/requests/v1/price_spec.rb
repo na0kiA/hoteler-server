7f987a3b79be07c0c78227bf04ccf80ecce775dd
na0kiA <na0ki199823@gmail.com>
2023-05-04 15:59:22 +0900

    [add]料金状況を確認するためのspecを追加

--- spec/requests/v1/price_spec.rb
+++ spec/requests/v1/price_spec.rb
@@ -4,6 +4,8 @@ require "rails_helper"
 
 RSpec.describe "V1::Price", type: :request do
   describe "GET /v1/search/price - v1_search_price#index" do
-    context "料金を検索できる場合"
+    context "料金を検索できる場合" do
+      
+    end
   end
 end
